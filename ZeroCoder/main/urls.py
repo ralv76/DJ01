911e7bb8a37a5de15db631d177a17fbb14b6420d
ralv76 <133973999+ralv76@users.noreply.github.com>
2025-10-17 00:46:34 +0300

    3
    3

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('new', views.new),
-    path('new2', views.new2),
-    path('new3', views.new3),
+    path('new', views.new, name='new'),
+    path('new2', views.new2, name='new2'),
+    path('new3', views.new3, name='new3'),
     path('data', views.data),
-    path('test', views.test)
+    path('test', views.test),
+    
 ]
