a3f6d2afd6ee9a803173813918930f5ac64f64df
ralv76 <133973999+ralv76@users.noreply.github.com>
2025-10-16 15:56:23 +0300

    2
    2

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('new', views.new),
+    path('new2', views.new2),
+    path('new3', views.new3),
     path('data', views.data),
     path('test', views.test)
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>HellO</h1>")
+    #return HttpResponse("<h1>HellO</h1>")
+    return render(request,'main/index.html')
 
 def new(request):
-    return HttpResponse("<h1>Page === New</h1>")
+    #return HttpResponse("<h1>Page === New</h1>")
+    return render(request,'main/new.html')
+
+def new2(request):
+    #return HttpResponse("<h1>Page === New</h1>")
+    return render(request,'main/new2.html')
+
+def new3(request):
+    #return HttpResponse("<h1>Page === New</h1>")
+    return render(request,'main/new3.html')
 
 def data(request):
     return HttpResponse("<h1>Page === Data</h1><a href=\"test\">На тест</a>")
